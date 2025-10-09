574fe8896d35bc6daa02203386bca7c17d9468da
Sharpor1 <marlonniloestefania@gmail.com>
2025-10-09 14:35:12 -0300

    admin

--- Siniestro/ClienteSiniestro/admin.py
+++ Siniestro/ClienteSiniestro/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Sinis
 
-# Register your models here.
+@admin.register(Sinis)
+class SinisAdmin(admin.ModelAdmin):
+    list_display = ('asunto', 'fecha', 'estado')
+    search_fields = ('asunto', 'descripcion', 'estado')
+    list_filter = ('estado', 'fecha')
