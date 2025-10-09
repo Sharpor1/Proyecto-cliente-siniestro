3bde789c0736d12a828a65feacdc36191da562af
Sharpor1 <marlonniloestefania@gmail.com>
2025-10-09 00:14:44 -0300

    calendario

--- Siniestro/ClienteSiniestro/forms.py
+++ Siniestro/ClienteSiniestro/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from .models import Sinis
 
 class SinisForm(forms.ModelForm):
-    
     class Meta:
         model = Sinis
         fields = '__all__'
+        widgets = {
+            'fecha': forms.DateInput(attrs={'type': 'date'})
+        }
